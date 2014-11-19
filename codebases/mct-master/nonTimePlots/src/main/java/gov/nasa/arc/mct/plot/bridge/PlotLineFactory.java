039103ed48e4d34e1cb91f02bfd65ee1c7e0ed12
Nastassia <tasha42092@hotmail.com>
2014-11-19 14:04:44 -0800

    Deleting unused codebases
    Deleting the code bases that we are not using for our project

--- codebases/mct-master/nonTimePlots/src/main/java/gov/nasa/arc/mct/plot/bridge/PlotLineFactory.java
+++
@@ -1,7 +0,0 @@
-package gov.nasa.arc.mct.plot.bridge;
-
-import gov.nasa.arc.mct.plot.adapter.PlotLine;
-
-public interface PlotLineFactory {
-	public PlotLine createLine();
-}
--- codebases/mct-master/nonTimePlots/src/main/java/gov/nasa/arc/mct/plot/data/DataReceiver.java
+++
@@ -1,5 +0,0 @@
-package gov.nasa.arc.mct.plot.data;
-
-public interface DataReceiver {
-	public void addData(double independent, double dependent);
-}
